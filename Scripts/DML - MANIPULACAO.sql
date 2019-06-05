c236fa7fbae4cf478f5ddb7d36efb064e49bd23f
Artur Locateli Ott <22972804830@info.local>
2019-06-05 11:27:37 -0300

    senoi-push

---
+++ Scripts/DML - MANIPULACAO.sql
@@ -0,0 +1,11 @@
+USE senai_Pizzarias_extra
+
+INSERT INTO USUARIO (EMAIL, SENHA) VALUES ('gandolf', '132'), ('VuuR@VuuR.com','VuuR')
+
+INSERT INTO PIZZARIAS (NOME, ISVEGAN, ENDERECO, TELEFONE_COMERCIAL)
+ VALUES ('Tarantella', 0 ,'Av. Barão de Limeira, ', '1188777788')
+      , ('Pizzeria', 0, '')
+	  , ('Picejo', 1 , '')
+
+select * from pizzarias
+
